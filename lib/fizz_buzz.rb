d27c0e91683399c5cce2c4818e876e52e3f2b975
George_Lewis <georgejlewis@hotmail.com>
2018-04-03 16:47:58 +0200

    Fizz-buzz add to git

---
+++ lib/fizz_buzz.rb
@@ -0,0 +1,14 @@
+# lib/fizz_buzz.rb
+
+def fizz_buzz(number)
+
+  if number % 15 == 0
+    'fizzbuzz'
+  elsif number % 3 == 0
+    'fizz'
+  elsif number % 5 == 0
+    'buzz'
+  else
+    number
+  end
+end

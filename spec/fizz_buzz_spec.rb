21b028a66634bf4abae9bb322c09985d2569444e
oliver_ochman <oliver.ochman@hotmail.com>
2018-04-04 09:39:16 +0200

    modify spec file

--- spec/fizz_buzz_spec.rb
+++ spec/fizz_buzz_spec.rb
@@ -11,7 +11,7 @@ describe 'fizz_buzz' do
   end
 
   it "returns 'buzz' if number is divisible by 5" do
-    expect(fizz_buzz(5)).to eq 'buzz'
+    expect(fizz_buzz(5)).to eq 'ruzz'
   end
 
   it "returns 'fizzbuzz' if number is divisible by 3 and 5" do
